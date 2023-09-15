4194a8436e020fe11436612addcba1b304b989ff
1082cubi <1082cubi@hanmail.net>
2023-09-15 17:41:15 +0900

    io

---
+++ src/ch18io/lecture/C21copy.java
@@ -0,0 +1,8 @@
+package ch18io.lecture;
+
+public class C21copy {
+    //BufferedInputStream
+    //BufferedOutputTtream
+    // 그림 파일 복사하는 코드
+
+}
---
+++ src/practice/Test2/Text3.java
@@ -0,0 +1,15 @@
+//package practice.Test2;
+//
+//public class Text3 {
+//    public static void main(String[] args) {
+//        A w =new A();
+//    }
+//
+//}
+//class A {
+//
+//   void getColor() {
+//
+//   }
+//}
+//
